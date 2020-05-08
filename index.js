1ee26dc57d805fb2c2b534e7f660942d7e9d0164
Stephane Bochet <s.bochet1@gmail.com>
2020-05-07 21:32:17 -0400

    Change earthquake json field from date to time.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Using node v10.15.0 (npm v6.4.1)
 //  $ node usgs_data_fetch.js
 // Earthquakes API example:
-// https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-12-01&endtime=2020-01-14&catalog=pr
+// https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-12-26&endtime=2020-07-16&catalog=pr&minmagnitude=0
 // starttime - Default now
 // endtime - Default 30 days ago
 
@@ -10,7 +10,7 @@ const fs = require('fs');
 const https = require('https');
 const BulkInsertEarthquakes = require('./mongodb/BulkInsertEarthquakes');
 
-// const requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-12-26&endtime=2020-07-16&catalog=pr&minmagnitude=0'
+// const requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-11-26&endtime=2020-07-16&catalog=pr&minmagnitude=0'
 const requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&catalog=pr&minmagnitude=0'
 https.get(requestURL, (resp) => {
   let data = '';
@@ -51,7 +51,7 @@ const transformEarthquakeData = (usgsEarthquakes) => {
     const transformedData = usgsEarthquakes.features.map(feature => {
         const slimFeature = {
             mag: feature.properties.mag,
-            date: feature.properties.time,
+            time: feature.properties.time,
             tz: feature.properties.tz,
             location : {"type" : "Point" , "coordinates" : [feature.geometry.coordinates[0], feature.geometry.coordinates[1]]},
             depth: feature.geometry.coordinates[2],
